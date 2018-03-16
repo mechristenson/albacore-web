f6a5665fa8401ca2910668e1914ac96098a61b16
Mark Christenson <mark.evan.christenson@gmail.com>
2018-03-15 23:53:06 -0700

    feat: support /spot/:id parameter
    - console log spot forecast for different id parameters

--- routes/spot.js
+++ routes/spot.js
@@ -2,12 +2,15 @@ var express = require('express');
 var request = require('superagent');
 var router = express.Router();
 
-var config = require('../config/dev.js');
-var secrets = require('../config/secrets.js');
+var mswController = require('../controllers/msw.js');
 
 /* GET spot listing. */
-router.get('/', function(req, res, next) {
-  res.render('spot', { spotId: '280'});
+router.get('/:id', function(req, res, next) {
+  var id = parseInt(req.params.id, 10);
+  var forecast = mswController.getSwellForecastForSpot(id);
+
+  res.render('spot', { spotId: id,
+                       response: forecast});
 });
 
 module.exports = router;
