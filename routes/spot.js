a40f94de744782301b9fbb35618737a93504734e
Mark Christenson <mark.evan.christenson@gmail.com>
2018-12-04 19:42:11 -0800

    feat: add caching skeleton to spot page

--- routes/spot.js
+++ routes/spot.js
@@ -1,28 +1,55 @@
 var express = require('express');
+var _ = require('underscore');
+
 var mswService = require('../services/msw.js');
 var router = express.Router();
+var db = require('../db.js')
 
 /* GET spot listing. */
 router.get('/:id', function(req, res, next) {
   var spotId = parseInt(req.params.id, 10);
 
-  mswService(spotId, function (forecast) {
-    res.render('spot', { spotId: spotId,
-                         spotName: "El Porto",
-                         timestamp: new Date(forecast[0].timestamp*1000),
-                         swellUnits: forecast[0].swell.unit,
-                         minSwellHeight: forecast[0].swell.minBreakingHeight,
-                         maxSwellHeight: forecast[0].swell.maxBreakingHeight,
-                         primarySwellHeight: forecast[0].swell.components.primary.height,
-                         primarySwellPeriod: forecast[0].swell.components.primary.period,
-                         primarySwellDirection: forecast[0].swell.components.primary.compassDirection,
-                         secondarySwellHeight: forecast[0].swell.components.secondary.height,
-                         secondarySwellPeriod: forecast[0].swell.components.secondary.period,
-                         secondarySwellDirection: forecast[0].swell.components.secondary.compassDirection,
-                         windUnits: forecast[0].wind.unit,
-                         windSpeed: forecast[0].wind.speed,
-                         windDirection: forecast[0].wind.compassDirection});
-                         // response: JSON.stringify(forecast)});
+  db.forecast.findOne({
+    where: {
+      spotId: spotId
+    }
+  }).then(function(forecast) {
+    if (!!forecast) {
+      // Found in DB
+      res.render('spot', { spotId: spotId,
+                            spotName: "Cache",
+                            timestamp: forecast.timestamp,
+                            swellUnits: forecast.swellUnits,
+                            minSwellHeight: forecast.minSwellHeight,
+                            maxSwellHeight: forecast.maxSwellHeight
+      });
+    } else {
+      // Not in DB, call service and save
+      mswService(spotId, function (forecast) {
+        var body = {
+          spotId: spotId,
+          spotName: "Web API",
+          timestamp: new Date(forecast[0].timestamp*1000),
+          swellUnits: forecast[0].swell.unit,
+          minSwellHeight: forecast[0].swell.minBreakingHeight,
+          maxSwellHeight: forecast[0].swell.maxBreakingHeight,
+        }
+
+        db.forecast.create(body).then(function(forecast) {
+          res.render('spot', { spotId: spotId,
+            spotName: "Web API",
+            timestamp: forecast.timestamp,
+            swellUnits: forecast.swellUnits,
+            minSwellHeight: forecast.minSwellHeight,
+            maxSwellHeight: forecast.maxSwellHeight
+          });
+        }, function(e) {
+          res.status(400).json(e);
+        });
+      });
+    }
+  }, function(e) {
+    res.status(500).send();
   });
 });
 
