ab45e09ebee1a48eab95b73144fd3e64333fcbe3
Mark Christenson <mark.evan.christenson@gmail.com>
2018-12-05 16:52:47 -0800

    feat: implement forecast model

--- models/forecast.js
+++ models/forecast.js
@@ -23,6 +23,43 @@ module.exports = function (sequelize, DataTypes) {
     maxSwellHeight: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+    // Convert Swell into separate model
+    primarySwellHeight: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    primarySwellPeriod: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    primarySwellDirection: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    secondarySwellHeight: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    secondarySwellPeriod: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    secondarySwellDirection: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    windUnits: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    windSpeed: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    windDirection: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
 };
